2c0b3ee30cec091e0bdc6f9200a572812063c233
SpaYco <zizou.aziz101@gmail.com>
2020-10-06 14:38:07 +0100

    added BookForm.js

---
+++ src/components/BookForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+const getCategories = () => {
+  const result = [];
+  for (let i = 0; i < categories.length; i += 1) {
+    result.push(<option value={categories[i]}>{categories[i]}</option>);
+  }
+  return result;
+};
+
+const BookForm = () => (
+  <form>
+    <label htmlFor="title">
+      title
+      <input id="title" name="title" type="text" />
+    </label>
+    <select name="categories" id="categories">
+      {getCategories()}
+    </select>
+    <input type="submit" />
+  </form>
+);
+
+export default BookForm;
