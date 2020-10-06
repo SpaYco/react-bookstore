9e60cbfae1ce38d020ecd811957968074e30279d
SpaYco <zizou.aziz101@gmail.com>
2020-10-06 14:08:43 +0100

    added BookList.js

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Book from './Book';
+
+const BookList = () => (
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+    <Book book={{ id: 0, title: 'harry potter', category: 'magic' }} />
+  </table>
+);
