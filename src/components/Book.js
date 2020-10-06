6f14e7aa1921a39e984dae88dd9ab6dbb0ef5248
SpaYco <zizou.aziz101@gmail.com>
2020-10-06 14:08:58 +0100

    added Book.js

---
+++ src/components/Book.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({ id, title, category }) => (
+  <tr>
+    <td>{id}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+);
+
+Book.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  category: PropTypes.string,
+};
+Book.defaultProps = {
+  id: 0,
+  title: 'invalid',
+  category: 'none',
+};
