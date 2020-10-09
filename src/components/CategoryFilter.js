ee8ca957d886f0aac2a9b720b66056066ae4492d
SpaYco <zizou.aziz101@gmail.com>
2020-10-09 14:47:39 +0100

    added category filter

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+const getCategories = () => {
+  const result = [];
+  for (let i = 0; i < categories.length; i += 1) {
+    result.push(
+      <option key={categories[i]} value={categories[i]}>
+        {categories[i]}
+      </option>,
+    );
+  }
+  return result;
+};
+
+class CategoryFilter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.bookFilter = this.bookFilter.bind(this);
+  }
+
+  bookFilter(event) {
+    const { changeFilter } = this.props;
+    changeFilter(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <select onChange={this.bookFilter} name="categories" id="categories" defaultValue="All">
+          {getCategories()}
+        </select>
+      </div>
+    );
+  }
+}
+
+CategoryFilter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,37 +3,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { removeBook } from '../actions/index';
+import { removeBook, changeFilter } from '../actions/index';
+import CategoryFilter from '../components/CategoryFilter';
 
-const BookList = ({ books, handleBookRemove }) => {
+const BookList = ({ books, handleBookRemove, filter, handleFilterChange }) => {
   const getBooks = () => {
     const result = [];
     for (let i = 0; i < books.length; i += 1) {
-      result.push(
-        <Book
-          key={i}
-          id={books[i].id}
-          title={books[i].title}
-          category={books[i].category}
-          removeOldBook={handleBookRemove}
-        />,
-      );
+      if (books[i].category === filter || filter === 'All') {
+        result.push(
+          <Book
+            key={i}
+            id={books[i].id}
+            title={books[i].title}
+            category={books[i].category}
+            removeOldBook={handleBookRemove}
+          />,
+        );
+      }
     }
     return result;
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove Book</th>
-        </tr>
-      </thead>
-      <tbody>{getBooks()}</tbody>
-    </table>
+    <div>
+      <CategoryFilter changeFilter={handleFilterChange} />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Remove Book</th>
+          </tr>
+        </thead>
+        <tbody>{getBooks()}</tbody>
+      </table>
+    </div>
   );
 };
 
@@ -46,17 +52,21 @@ BookList.propTypes = {
     }),
   ),
   handleBookRemove: PropTypes.func.isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
 };
 
 BookList.defaultProps = {
   books: [],
 };
 
-const mapStateToProps = state => ({ books: state.books });
+const mapStateToProps = state => ({ books: state.books, filter: state.filter });
 
 const mapDispatchToProps = dispatch => ({
-  handleBookRemove: state => {
-    dispatch(removeBook(state));
+  handleBookRemove: book => {
+    dispatch(removeBook(book));
+  },
+  handleFilterChange: filter => {
+    dispatch(changeFilter(filter));
   },
 });
 
